29840495c6c62b5fa3824cfb77408f0717e806e9
KiryhaPikoff <pikov_kirya@mail.ru>
2020-08-25 22:15:57 +0300

    Create task tests and sequences.

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,10 +1,29 @@
 FactoryBot.define do
   sequence :string,
-           aliases: [:first_name, :last_name, :password, :avatar] do |n|
+           aliases: [:first_name,
+                     :last_name,
+                     :password,
+                     :avatar,
+                     :name,
+                     :description] do |n|
     "string#{n}"
   end
 
   sequence :email do |n|
     "person#{n}@example.com"
+  end
+
+  sequence :state do
+    ['new_task',
+     'in_development',
+     'in_qa',
+     'in_code_review',
+     'ready_for_release',
+     'released',
+     'archived'].sample
+  end
+
+  sequence :date, aliases: [:expired_at] do |n|
+    Date.today + n.days
   end
 end
